2a5e3391e11f2b88b384c63f6985997f168dd96a
Trevor Youmans <corruptedconsciousness@proton.me>
2025-11-20 18:33:30 -0500

    Modify hello.c to greet user by name

--- week1/hello.c
+++ week1/hello.c
@@ -1,10 +1,8 @@
-// hello.c
-// CS50x Week 1 – Introduction to C
-// Prints a simple greeting.
-
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string name = get_string("what's your name? ");
+    printf("hello, %s\n", name);
 }
